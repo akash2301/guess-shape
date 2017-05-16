8e8088817d8493cea924829e3434a941a72a2ffe
Raghav <duaraghav8@gmail.com>
2017-05-16 20:00:25 +0530

    fixed computeAngle()

--- shape.cpp
+++ shape.cpp
@@ -5,6 +5,7 @@
 #include <cmath>
 #include <cstdlib>
 #define PI 3.14159265
+#define SMALL_NUMBER 1e-5
 
 using namespace std;
 
@@ -24,12 +25,22 @@ bool areFloatValuesSimilar (float a, float b) {
 
 float computeAngle (Coord mid, Coord left, Coord right) {
 	const float deg = 180.0f / PI;
+
+	// x values of 2 points are same, subtract a small number to the non-mid point to avoid divide-by-zero
+	if (left.x == mid.x) {
+		left.x -= SMALL_NUMBER;
+	}
+
+	if (right.x - mid.x) {
+		right.x -= SMALL_NUMBER;
+	}
+
 	float gradML = (left.y - mid.y) / (left.x - mid.x),
 		gradMR = (mid.y - right.y) / (mid.x - right.x);
-	float thetaML = atan (gradML) * deg, thetaMR = atan (gradMR) * deg;
-	float angle = abs (thetaML - thetaMR);
+	float eq = abs ((gradML - gradMR) / (1+(gradMR*gradML)));
 
-	return min (angle, 180 - angle);
+	//return min (angle, 180 - angle);
+	return atan (eq) * deg;
 }
 
 vector< float > getInteriorAngles (vector< Coord >& coords) {
@@ -37,10 +48,11 @@ vector< float > getInteriorAngles (vector< Coord >& coords) {
 	const unsigned int numOfCoords = coords.size ();
 
 	// First point is a special case, handle it differently
-	// For rest of points, take the point, take 1 before it & 1 after it & compute angle (after gradients)
-
+	// Since the last point on list is same as 1st point (to ensure closed shape), use second-last point
+	// as the right point of the first point.
 	angles.push_back (computeAngle(coords [0], coords [1], coords [numOfCoords - 2]));
 
+	// For rest of points, take the point, take 1 before it & 1 after it & compute angle (after gradients)
 	for (int i = 1; i < numOfCoords - 1; i++) {
 		angles.push_back (computeAngle (coords [i], coords [i-1], coords [i+1]));
 	}
@@ -53,12 +65,27 @@ string identifyTriangle (vector< Coord >& coords) {
 	const vector< float > interiorAngles = getInteriorAngles (coords);
 	unsigned int flags = 0;
 
-	if (areFloatValuesSimilar (interiorAngles [0], interiorAngles [1]) &&
-		areFloatValuesSimilar (interiorAngles [0], interiorAngles [2])) {
+	if (areFloatValuesSimilar (interiorAngles [0], interiorAngles [1])) {
+		flags |= 1;
+	}
+
+	if (areFloatValuesSimilar (interiorAngles [0], interiorAngles [2])) {
+		flags |= 2;
+	}
+
+	if (areFloatValuesSimilar (interiorAngles [1], interiorAngles [2])) {
+		flags |= 4;
+	}
+
+	if (!flags) {
+		return "Scalene " + triangle;
+	}
+
+	if (flags == 7) {
 		return "Equilateral " + triangle;
 	}
 
-	return triangle;
+	return "Isoceles " + triangle;
 }
 
 
@@ -79,7 +106,7 @@ string detectShapeDecisionTree (vector< Coord >& coords) {
 	const string notAValidShape = "Not a valid Shape";
 
 	switch (coords.size ()) {
-		case 0:
+		case 0: case 3:
 			return notAValidShape;
 		case 1:
 			return "Point";
